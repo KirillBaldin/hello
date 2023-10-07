75b619cf9979de5c9b4b602ef247a746ac9552dd
Кирилл Балдин <baldinkirill58@gmail.com>
2023-10-07 16:07:51 +0300

    Внес изменения

---
+++ hello.pas
@@ -0,0 +1,19 @@
+﻿{Измените код в файле hello.pas, чтобы программа спрашивала у пользователя язык, 
+на котором он говорит и выводила приветствие на этом языке}
+begin
+  var language := ReadString('Напишите свой язык:');
+  if language = 'Русский' then
+    print('Привет!');
+  if language = 'English' then
+    print('Hello!');
+  if language = 'Deutsch' then
+    print('Hallo!')
+end.
+{
+Напишите свой язык: English
+Hello! 
+Напишите свой язык: Русский
+Привет! 
+Напишите свой язык: Deutsch
+Hallo! 
+}
